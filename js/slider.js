d0b72cc2496555c640a7bd9fb58091d6c7df5db5
RomaHomaN9 <Cod_text@ukr.net>
2025-05-25 11:06:26 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+
+    slidesPerView: 1,
+    spaceBetween: 16,
+
+    breakpoints: {
+        480: {
+            slidesPerView: 2
+        },
+        768: {
+            slidesPerView: 3
+        }
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        balletClass: 'swiper-pagination-bullet',
+        balletActiveClass: 'swiper-pagination-bullet-active'
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    }
+});
