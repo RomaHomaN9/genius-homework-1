88e38ccb30cecb49da7247069c3025eed6cc8a70
RomaHomaN9 <Cod_text@ukr.net>
2025-06-11 22:17:06 +0300

    Delete js directory

--- js/modal-tel.js
+++
@@ -1,9 +0,0 @@
-const modalWindowTel = document.getElementsByClassName('modal-tel')[0];
-
-function openModalWindowTel() {
-    modalWindowTel.classList.remove('hidden');
-}
-
-function closeModalWindowTel() {
-    modalWindowTel.classList.add('hidden');
-}
--- js/modal-window.js
+++
@@ -1,9 +0,0 @@
-const modalWindow = document.getElementsByClassName('backdrop')[0];
-
-function openModalWindow() {
-    modalWindow.classList.remove('hidden');
-}
-
-function closeModalWindow() {
-    modalWindow.classList.add('hidden');
-}
